8f0a14b2cea6c474cd775fb0a982f92781209d34
Gayana-19 <64000886+Gayana-19@users.noreply.github.com>
2025-01-30 02:02:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import ListsPage from "./components/ListsPage";
 import ListCreationPage from "./components/ListCreationPage";
 import "./App.css";
